f50f12b3783e028cae2383a97ffe9ee915827294
Cantika Putri Arbiliansyah <85111014+cantikaputria@users.noreply.github.com>
2023-07-20 09:38:01 +0700

    Delete Pertemuan 4 Kuis 1 directory

--- Pertemuan 4 Kuis 1/Kuis No 2/mesin.c
+++
@@ -1,8 +0,0 @@
-/* Saya Cantika Putri Arbiliansyah dengan NIM [2103727] mengerjakan 
-Soal Kuis Nomor 1 dalam mata kuliah Algoritma dan Pemrograman 2
-untuk keberkahan-Nya maka saya tidak melakukan kecurangan seperti 
-yang telah dispesifikasikan. Aamiin.
-*/
-
-//LIBRARY
-#include "header.h"
